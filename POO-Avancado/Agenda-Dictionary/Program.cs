0d40acfc6af2bdc4e1edfafdd85a9999ca49e21b
Nickolas Passos Homem de Mello <nickolasphmello10@gmail.com>
2025-10-06 22:09:43 -0300

    Lists && Dictionarys

---
+++ POO-Avancado/Agenda-Dictionary/Program.cs
@@ -0,0 +1,87 @@
+﻿Dictionary<string, string> AgendaTelefonica = new Dictionary<string, string>();
+
+int opcao = 0;
+
+do
+{
+  Console.WriteLine();
+  Console.WriteLine("\nAGENDA TELEFONICA");
+  Console.WriteLine("1 - Inserir contato");
+  Console.WriteLine("2 - Remover contato");
+  Console.WriteLine("3 - Consultar contato");
+  Console.WriteLine("4 - Finalizar programa");
+  opcao = int.Parse(Console.ReadLine()!);
+  Console.WriteLine();
+
+  switch (opcao)
+  {
+    case 1:
+      Console.WriteLine("Qual o nome deseja salvar o contato? ");
+      string nomeContato = Console.ReadLine()!;
+
+      Console.WriteLine($"Qual o telefone do/da {nomeContato}? ");
+      string telefoneContato = Console.ReadLine()!;
+
+      if (AgendaTelefonica.ContainsKey(nomeContato))
+      {
+        Console.WriteLine("Esse contato já esta cadastrado!, iremos atualizar o seu número!");
+        AgendaTelefonica[nomeContato] = telefoneContato;
+      }
+      else
+      {
+        AgendaTelefonica.Add(nomeContato, telefoneContato);
+        Console.WriteLine("Contato Registrado!");
+      }
+      break;
+
+    case 2:
+      if (AgendaTelefonica.Count == 0)
+      {
+        Console.WriteLine("Não é possível remover pois não possui nenhum contato!");
+        break;
+      }
+
+      Console.WriteLine("Qual o nome do contato que você deseja remover? ");
+      string nomeRemover = Console.ReadLine()!;
+
+      if (AgendaTelefonica.ContainsKey(nomeRemover))
+      {
+        AgendaTelefonica.Remove(nomeRemover);
+        Console.WriteLine("Contato removido com sucesso!");
+      }
+      else
+      {
+        Console.WriteLine("Contato não encontrado!");
+      }
+      break;
+
+    case 3:
+      if (AgendaTelefonica.Count == 0)
+      {
+        Console.WriteLine("Não é possível CONSULTAR pois não possui nenhum contato!");
+        break;
+      }
+
+      Console.WriteLine("Qual o nome do contato que você deseja consultar? ");
+      string nomeConsulta = Console.ReadLine()!;
+
+      if (AgendaTelefonica.ContainsKey(nomeConsulta))
+      {
+        Console.WriteLine($"Contato {nomeConsulta} - {AgendaTelefonica[nomeConsulta]}");
+      }
+      else
+      {
+        Console.WriteLine("Contato não encontrado!");
+      }
+      break;
+
+    case 4:
+      break;
+
+    default:
+      Console.WriteLine("Opção inválida!");
+      break;
+  }
+} while (opcao != 4);
+
+Console.WriteLine("----------------- PROGRAMA FINALIZADO -----------------");
---
+++ POO-Avancado/Tarefas-List/Program.cs
@@ -0,0 +1,78 @@
+﻿List<string> ListaTarefas = new List<string>();
+
+int opcao = 0;
+
+do
+{
+  Console.WriteLine();
+  Console.WriteLine("\nLISTA DE TAREFAS");
+  Console.WriteLine("1 - Adicionar Tarefa");
+  Console.WriteLine("2 - Remover Tarefa");
+  Console.WriteLine("3 - Ver Tarefas");
+  Console.WriteLine("4 - Finalizar programa");
+  opcao = int.Parse(Console.ReadLine()!);
+  Console.WriteLine();
+
+  switch (opcao)
+  {
+    case 1:
+      Console.WriteLine("Qual a TAREFA você deseja adicionar? ");
+      string tarefa = Console.ReadLine()!;
+
+
+      if (ListaTarefas.Contains(tarefa))
+      {
+        Console.WriteLine("Essa Tarefa já existe!");
+        break;
+      }
+      else
+      {
+        ListaTarefas.Add(tarefa);
+        Console.WriteLine("Tarefa Registrada!");
+      }
+      break;
+
+    case 2:
+      if (ListaTarefas.Count == 0)
+      {
+        Console.WriteLine("Não é possível REMOVER pois não possui nenhuma TAREFA!");
+        break;
+      }
+
+      Console.WriteLine("Qual o nome da TAREFA que você deseja remover?");
+      string tarefaRemover = Console.ReadLine()!;
+
+      if (tarefaRemover.Contains(tarefaRemover))
+      {
+        ListaTarefas.Remove(tarefaRemover);
+        Console.WriteLine("Tarefa removida com Sucesso!");
+      }
+      else
+      {
+        Console.WriteLine("Tarefa não encontrada!");
+      }
+      break;
+
+    case 3:
+      if (ListaTarefas.Count == 0)
+      {
+        Console.WriteLine("Não é possível CONSULTAR pois não possui nenhuma!");
+        break;
+      }
+      
+      foreach (var i in ListaTarefas)
+      {
+        Console.WriteLine($"Tarefa - {i}");
+      }
+      break;
+
+    case 4:
+      break;
+
+    default:
+      Console.WriteLine("Opção inválida!");
+      break;
+  }
+} while (opcao != 4);
+
+Console.WriteLine("----------------- PROGRAMA FINALIZADO -----------------");
